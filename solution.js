5b1dc92e2b0f177799faa9d3f565a99e82c0ee00
Rami <103028944+Rami24t@users.noreply.github.com>
2022-07-12 12:40:42 +0200

    Create solution.js

---
+++ solution.js
@@ -0,0 +1,22 @@
+let i=0;
+while(i<15){
+console.log(i);
+i++;
+}
+
+let sum=0;
+let n=1;
+while(n<21){
+sum+=n;
+n++;
+}
+console.log('Sum of numbers from 1 to 20 is', sum);
+
+i=0;
+do
+{
+console.log('The number is',i);
+i++;
+}
+while(i<20)
+
